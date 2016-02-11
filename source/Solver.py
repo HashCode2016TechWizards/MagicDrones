9accf574cb03c6a0fc4c33c32f28d5b43834b113
MagusDei <jami.valorinta@helsinki.fi>
2016-02-11 22:21:23 +0200

    Added dronesNeeded to Solver.py

--- source/Solver.py
+++ source/Solver.py
@@ -1,3 +1,14 @@
 
+import math
+
 class Solver:
+
+    maxload = 1
+    products = {}
+
+    def dronesNeeded(order):
+        # Get the sum of item weights for this order
+        sumweight = sum([products[i] for i in order.items])
+        return math.ceil(sumweight/maxload)
+
     pass
