a0bc671428e8a7b669c605c246e498d121aea6f7
Yskinator <yski2moreletters@gmail.com>
2016-02-11 22:31:52 +0200

    Added sortByDistance to Solver.

--- source/Solver.py
+++ source/Solver.py
@@ -1,3 +1,13 @@
+from Warehouse import Warehouse
+import math
 
 class Solver:
-    pass
+
+    def sortByDistance(self, position, items):
+        sortedItems = []
+        for item in items:
+            distance = math.sqrt((position[0]-item.position[0])**2 + (position[1]-item.position[1])**2)
+            sortedItems.append((distance,item))
+        sortedItems.sort()
+        return sortedItems
+
